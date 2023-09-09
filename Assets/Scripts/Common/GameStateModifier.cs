173c4996adbde94ecbff1bf6d44322a4890bd7ab
Lucy Daniels <lucy.daniels.software@gmail.com>
2023-09-09 11:18:06 +0100

    import old scripts (#5)

---
+++ Assets/Scripts/Common/GameStateModifier.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using CommonComponents.Interfaces;
+using Management;
+using UnityEngine;
+
+namespace CommonComponents
+{
+    public class GameStateModifier : MonoBehaviour, IGameStateSubscriber
+	{
+		[SerializeField] private List<GameState> activeStateList;
+		private MonoBehaviour[] _behaviourList;
+		private void Start()
+		{
+			GameStateManager.Instance.Subscribe(this);
+			_behaviourList = GetComponents<MonoBehaviour>();
+		}
+
+		private void OnDestroy()
+		{
+			GameStateManager.Instance.Unsubscribe(this);
+		}
+
+		public void OnStateChange(GameState newState)
+		{
+			bool enable = activeStateList.Contains(newState);
+			foreach (var mb in _behaviourList)
+			{
+				mb.enabled = enable;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Common/InteractableActor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace CommonComponents.Interfaces
+{
+	public class InteractableActor : MonoBehaviour
+	{
+		protected Interactable InteractiveObject;
+
+		public void SetInteractableObject(Interactable interactiveObject)
+		{
+			InteractiveObject = interactiveObject;
+			Debug.Log($"CurrentObject = {InteractiveObject}");
+
+		}
+
+		public void ResetInteractableObject(Interactable interactable)
+		{
+			if (InteractiveObject == interactable)
+			{
+				InteractiveObject = null;
+			}
+		}
+
+		public bool ActionCurrent()
+		{
+			if (InteractiveObject != null)
+			{
+				return InteractiveObject.Action(this);
+			}
+
+			return false;
+		}
+	}
+}
