84afb141620273ec3860dd0e7e7f6f99779597ec
Olddevbless <edevilbless@gmail.com>
2023-09-10 19:24:44 +0300

    NPC UI

--- Assets/Scripts/NPCCharacterUI.cs
+++ Assets/Scripts/NPCCharacterUI.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCCharacterUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] TextMeshProUGUI firstName, lastName, level, npcClass, race, strength, dexterity, constitution, intelligence, wisdom, charisma;
+    [SerializeField] TextMeshProUGUI health_points, health_points_max, armor_class;
+    [SerializeField] TextMeshProUGUI traits, flaws, ideals, skills;
+    Canvas npcharacterSheetCanvas;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
     }
+    public void OnInteraction()
+    {
+        if (npcharacterSheetCanvas.enabled== false) 
+        {
+            npcharacterSheetCanvas.enabled = true;
+        }
+        else
+        {
+            npcharacterSheetCanvas.enabled = false;
+        }
+        
+    }
+    
 }
