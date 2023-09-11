2e5a41310e14eed2061d1070996079ca0927a4d0
Olddevbless <edevilbless@gmail.com>
2023-09-11 16:26:44 +0300

    some refactoring

--- Assets/Scripts/NPCS/CharacterSheet.cs
+++ Assets/Scripts/NPCS/CharacterSheet.cs
@@ -5,31 +5,31 @@ public class CharacterSheet : ScriptableObject
 {
 	
 	[Header("Character Details")]
-	[SerializeField] public string firstName;
-	[SerializeField] public string lastName;
-	[SerializeField] public string race;
+	public string firstName;
+	public string lastName;
+	public string race;
 
 	[Header("Character Class")]
-	[SerializeField] public int level;
-	[SerializeField] public string npcClass;
-	[SerializeField] public string[] skills;
+	public int level;
+	public string npcClass;
+	public string[] skills;
 
 	
 	[Header("Stats")]
-	[SerializeField] public int healthPoints;
-	[SerializeField] public int healthPointsMax;
-	[SerializeField] public int armorClass;
-	[SerializeField] public int strength;
-	[SerializeField] public int dexterity;
-	[SerializeField] public int constitution;
-	[SerializeField] public int intelligence;
-	[SerializeField] public int wisdom;
-	[SerializeField] public int charisma;
+	public int healthPoints;
+	public int healthPointsMax;
+	public int armorClass;
+	public int strength;
+	public int dexterity;
+	public int constitution;
+	public int intelligence;
+	public int wisdom;
+	public int charisma;
 
 	
 	[Header("Personality")]
-	[SerializeField] public string[] traits;
-	[SerializeField] public string[] flaws;
-	[SerializeField] public string[] ideals;
+	public string[] traits;
+	public string[] flaws;
+	public string[] ideals;
 
 }
