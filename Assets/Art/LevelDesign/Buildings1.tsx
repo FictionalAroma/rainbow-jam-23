1bf0b2fb92809fb876725910e246d1bb7748f984
Olddevbless <edevilbless@gmail.com>
2023-09-16 21:00:52 +0300

    leveldesign start

--- Assets/Art/LevelDesign/Buildings1.tsx
+++ Assets/Art/LevelDesign/Buildings1.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Buildings1" tilewidth="32" tileheight="32" tilecount="1435" columns="35">
+<tileset version="1.10" tiledversion="1.10.2" name="Buildings1" tilewidth="32" tileheight="32" tilecount="1435" columns="35" fillmode="preserve-aspect-fit">
  <image source="../../Packages/CL_v1.12/32x32/CL_Buildings.png" width="1120" height="1312"/>
  <tile id="490">
   <objectgroup draworder="index" id="2">
---
+++ Assets/Art/LevelDesign/Outdoors1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Outdoors" tilewidth="32" tileheight="32" tilecount="3960" columns="55">
+ <image source="../../Packages/EPIC RPG World Pack - Grass Land 2.0 v1.4/Tilesets/Tileset-Terrain.png" width="1760" height="2304"/>
+</tileset>
