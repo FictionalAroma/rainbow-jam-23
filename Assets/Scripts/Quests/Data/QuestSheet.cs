09cf5e18c6e5075d5a08cd08615e3580ef51f7cc
Olddevbless <edevilbless@gmail.com>
2023-09-23 16:10:10 +0300

    all npc animations

---
+++ Assets/Scripts/Quests/Data/QuestSheet.cs
@@ -0,0 +1,13 @@
+using System;
+using ExtensionClasses;
+using UnityEngine;
+
+[Serializable]
+//[CreateAssetMenu(fileName = "NewCharacter", menuName = "Character/New Character")]
+public class QuestSheet
+{
+	
+	
+
+	
+}
